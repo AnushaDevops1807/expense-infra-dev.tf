0da9a6d8e7551222558b61f512bcc8a153576884
Anusha N <nanu18071989@gmail.com>
2025-02-20 18:57:50 +0800

    cloudfrontdistribution

---
+++ 100-cdn/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+
+variable "zone_id" {
+    default = "Z093290231LHJLHYH7DM"
+}
+
+variable "domain_name" {
+    default = "anudevops.online"
+}
